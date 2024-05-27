68736fae9aa6390ff589ec75ca7109901cdac2a2
a-moseman <drewmoseman@gmail.com>
2024-05-27 18:07:14 -0400

    Refactor rotation function out of VectorMath and into Vector

--- src/main/java/org/amoseman/generativeart/vector/Vector.java
+++ src/main/java/org/amoseman/generativeart/vector/Vector.java
@@ -8,4 +8,25 @@ public final class Vector {
         this.x = x;
         this.y = y;
     }
+
+    public Vector add(Vector other) {
+        return new Vector(x + other.x, y + other.y);
+    }
+
+    public Vector subtract(Vector other) {
+        return new Vector(x - other.x, y - other.y);
+    }
+
+    public Vector rotate(Vector center, double radians) {
+        double s = Math.sin(radians);
+        double c = Math.cos(radians);
+
+        Vector p1 = subtract(center);
+        Vector p2 = new Vector(
+                p1.x * c - p1.y * s,
+                p1.x * s + p1.y * c
+        );
+        p2 = p2.add(center);
+        return p2;
+    }
 }
