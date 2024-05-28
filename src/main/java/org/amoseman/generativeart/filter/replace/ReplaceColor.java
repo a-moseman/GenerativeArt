f612fa8e921f666ed672263a8370bfdd247388ab
a-moseman <drewmoseman@gmail.com>
2024-05-28 17:37:05 -0400

    Refactor ColorToAlpha into ReplaceColor

---
+++ src/main/java/org/amoseman/generativeart/filter/replace/ReplaceColor.java
@@ -0,0 +1,29 @@
+package org.amoseman.generativeart.filter.replace;
+
+import org.amoseman.generativeart.filter.Filter;
+import org.amoseman.generativeart.image.ColorValue;
+import org.amoseman.generativeart.image.ImageData;
+
+import java.util.random.RandomGenerator;
+
+public class ReplaceColor implements Filter {
+    private final ColorValue target;
+    private final ColorValue replacement;
+    private final double threshold;
+
+    public ReplaceColor(ColorValue target, ColorValue replacement, double threshold) {
+        this.target = target;
+        this.replacement = replacement;
+        this.threshold = threshold;
+    }
+
+    @Override
+    public void apply(ImageData data, RandomGenerator random) {
+        for (int i = 0; i < data.getSize(); i++) {
+            ColorValue current = data.get(i);
+            if (current.distance(target) < threshold) {
+                data.set(i, replacement);
+            }
+        }
+    }
+}
