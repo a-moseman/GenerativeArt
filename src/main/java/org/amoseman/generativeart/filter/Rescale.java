bf479f41eef09078c3687ffc237a4fecd8dd6fc5
a-moseman <drewmoseman@gmail.com>
2024-05-28 10:02:10 -0400

    Implement Rescale filter

---
+++ src/main/java/org/amoseman/generativeart/filter/Rescale.java
@@ -0,0 +1,43 @@
+package org.amoseman.generativeart.filter;
+
+import org.amoseman.generativeart.ColorValue;
+import org.amoseman.generativeart.image.ImageData;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+public class Rescale implements Filter {
+    private final int scale;
+
+    public Rescale(int scale) {
+        this.scale = scale;
+    }
+
+    @Override
+    public void apply(ImageData data, Random random) {
+        ColorValue[] scaled = new ColorValue[data.getSize()];
+        for (int x = 0; x < data.getWidth(); x += scale) {
+            for (int y = 0; y < data.getHeight(); y += scale) {
+                ColorValue value = data.get(x, y);
+                for (int dx = 0; dx < scale; dx++) {
+                    for (int dy = 0; dy < scale; dy++) {
+                        int px = x + dx;
+                        if (px >= data.getWidth()) {
+                            continue;
+                        }
+                        int py = y + dy;
+                        if (py >= data.getHeight()) {
+                            continue;
+                        }
+                        scaled[px + py * data.getWidth()] = value;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < data.getSize(); i++) {
+            data.set(i, scaled[i]);
+        }
+
+    }
+}
